90c0197b7fc0c7d5554d38e44f181ed5c3a92596
Robert Champion <86251989+Champ32@users.noreply.github.com>
2022-07-31 10:52:26 -0400

    created team-class table

---
+++ Final/Models/TeamClass.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class TeamClass
+{
+    public int TeamClassId { get; set; }
+    public int TeamNameId { get; set; }
+    public string ClassName { get; set; }
+    public int Grade { get; set; }
+    public string YearStarted { get; set; }
+}
