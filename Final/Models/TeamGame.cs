e414826ade118caf9c4ab7f83b05fc9737a11529
Robert Champion <86251989+Champ32@users.noreply.github.com>
2022-07-31 10:51:38 -0400

    Created team-game table

---
+++ Final/Models/TeamGame.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class TeamClass
+{
+    public int TeamGameId { get; set; }
+    public int TeamNameId { get; set; }
+    public string GameName { get; set; }
+    public string YearStarted { get; set; }
+    public string Frequency { get; set; }
+}
