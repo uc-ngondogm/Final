098f04af51a3dce7d862e6e49d67feb77e2f1e4f
Robert Champion <86251989+Champ32@users.noreply.github.com>
2022-07-31 10:56:42 -0400

    Updated the naming conventions

--- Final/Data/ProjectContext.cs
+++ Final/Data/ProjectContext.cs
@@ -17,6 +17,17 @@ namespace Final
                 new TeamHobby {TeamHobbyId = 1,TeamNameId=1, HobbyName = "Martial Art", YearStarted ="04/04/1999" ,Frequency = "4/Month",Location = "Fairfield" },
                 new TeamHobby {TeamHobbyId = 2,TeamNameId=2, HobbyName = "Guitar", YearStarted ="10/30/2009" ,Frequency = "8/Month",Location = "Home" },
                 new TeamHobby {TeamHobbyId = 3,TeamNameId=3, HobbyName = "Hiking", YearStarted ="10/30/2009" ,Frequency = "8/Month",Location = "Park" });
+
+            builder.Entity<TeamClass>().HasData(
+                new TeamClass {TeamClassId = 1,TeamNameId=1, ClassName = "Contemperary Programming", Grade = 100 ,YearStarted = "5/8/2022"},
+                new TeamClass {TeamClassId = 2,TeamNameId=2, ClassName = "Contemperary Programming", Grade = 100 ,YearStarted = "5/8/2022"},
+                new TeamClass {TeamClassId = 3,TeamNameId=3, ClassName = "Contemperary Programming", Grade = 100 ,YearStarted = "5/8/2022"});
+
+            builder.Entity<TeamGame>().HasData(
+                new TeamGame {TeamGameId = 1,TeamNameId=1, GameName = "Call of Duty Black Ops 2", YearStarted = "5/8/2022", Frequency = "4/month"},
+                new TeamGame {TeamGameId = 2,TeamNameId=2, GameName = "Minecraft", YearStarted = "5/8/2022", Frequency = "12/month"},
+                new TeamGame {TeamGameId = 3,TeamNameId=3, GameName = "Far Cry 3", YearStarted = "5/8/2022", Frequency = "8/month"});
+
         }
 
         internal object GetNames(int id)
@@ -26,5 +37,7 @@ namespace Final
 
         public DbSet<TeamName> Names {get; set;}  
         public DbSet<TeamHobby> Hobbies {get; set;} 
+        public DbSet<TeamClass> Classes {get; set;}
+        public DbSet<TeamGame> Games {get; set;}
     }
 }
